40949290136897ac9488d9843677f39d3e646611
bald-win <1351533120@qq.com>
2019-04-22 22:51:28 +0800

    cart生成订单

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -1,54 +1,8 @@
 <script>
-var books = [
-  {
-    no: 1,
-    name: "The_Man_Who_Changed_China",
-    imgsrc: require("../assets/The_Man_Who_Changed_China.jpg"),
-    rawimg: '<img src="../assets/The_Man_Who_Changed_China.jpg">',
-    author: "罗伯特·劳伦斯·库恩",
-    ISBN: "7-5327-3655-5/K·154"
-  },
-  {
-    no: 2,
-    name: "Computer Systems A Programmer's Perspective 3rd",
-    imgsrc: require("../assets/csapp.jpg"),
-    rawimg: '<img src="../assets/csapp.jpg">',
-    author: "兰德尔 E. 布莱恩特",
-    ISBN: "978-7-111-56127-9"
-  },
-  {
-    no: 3,
-    name: "C++ Primer",
-    imgsrc: require("../assets/cppp.jpg"),
-    rawimg: '<img src="../assets/cppp.jpg">',
-    author: "Stanley B. Lippman",
-    ISBN: "978-7-121-15535-2"
-  },
-  {
-    no: 4,
-    name: "Database System Concepts 6th",
-    imgsrc: require("../assets/dsc.jpg"),
-    rawimg: '<img src="../assets/dsc.jpg">',
-    author: "Abraham Silberschatz",
-    ISBN: "978-7-04-031175-4"
-  }
-];
-var username = "";
-var bookdetail = {
-  no: "",
-  name: "",
-  imgsrc: "",
-  rawimg: "",
-  author: "",
-  ISBN: ""
-};
+var userid = "";
 var mycart = [];
-var bookinfo;
 export default {
-  books,
-  username,
-  bookdetail,
-  mycart,
-  bookinfo
+  userid,
+  mycart
 };
 </script>
--- src/views/MyCart.vue
+++ src/views/MyCart.vue
@@ -7,25 +7,38 @@
       <div class="cartinfo">
         <p>书名:{{item.name}}</p>
         <p>作者:{{item.author}}</p>
-        <p>ISBN:{{item.ISBN}}</p>
+        <p>ISBN:{{item.isbn}}</p>
+        <p>价格:{{item.price}}</p>
       </div>
       <div class="cartbutton">
         <el-button @click="cartdelete(item)">删除</el-button>
       </div>
       <div style="clear:both"></div>
     </div>
+    <div>
+      <el-button @click="clear">清空</el-button>
+      <el-button @click="submit">提交订单</el-button>
+    </div>
   </div>
 </template>
 
 <script>
+import { truncate } from "fs";
 export default {
   name: "MyCart",
   data: function() {
     return {
-      isempty: true,
-      cart: this.$global.mycart
+      cart: this.$global.mycart,
+      // iscartempty: 0
     };
   },
+  // computed: {
+  //   iscartempty: function() {
+  //     var cat = this.cart;
+  //     if (this.cart.lenth === 0) return true;
+  //     return false;
+  //   }
+  // },
   methods: {
     cartdelete(book) {
       for (var i = 0; i < this.$global.mycart.length; i++) {
@@ -35,6 +48,20 @@ export default {
         }
       }
       this.cart = this.$global.mycart;
+    },
+    clear: function() {
+      this.$global.mycart = [];
+      this.cart = this.$global.mycart;
+    },
+    submit: function() {
+      var cart = this.$global.mycart;
+      this.$http({
+        method:"post",
+        url:"http://localhost:8082/ebook/order/add",
+        data:cart
+      }).then(response=>{
+        console.log(response.data);
+      })
     }
   }
 };
